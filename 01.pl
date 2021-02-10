5bcd91a6c632b5bdf2e2290a690eef41e14d877b
Hiusu <luis_vala_junior@hotmail.com>
2021-02-10 02:00:02 -0300

    leves mudanças

--- 01.pl
+++ 01.pl
@@ -1,108 +1,92 @@
-genitor(pam,bob).
-
-genitor(tom,bob).
-
-genitor(tom,liz).
-
-genitor(bob,ann).
-
-genitor(bob,pat).
-
-genitor(pat,jim).
-
-
-
-
-genitor(gabriel,hiusu).
-
-genitor(madalena,hiusu).
-
-genitor(gabriel,guga).
-
-genitor(madalena,francisca).
-
-genitor(samuel,duda).
-
-genitor(samanta,duda).
-
-genitor(hiusu,montekyo).
-
-genitor(hiusu,nina).
-
-genitor(duda,montekyo).
-
-genitor(duda,nina).
-
-
+mulher(pessoa).
+mulher(pam).
+mulher(liz).
+mulher(pat).
+mulher(ann).
 mulher(madalena).
-
 mulher(samanta).
-
 mulher(francisca).
-
 mulher(duda).
-
 mulher(nina).
 
-
-
-
-mulher(pam).
-
-mulher(liz).
-
-mulher(pat).
-
-mulher(ann).
-
-
-
-
+homem(pessoa).
+homem(tom).
+homem(bob).
+homem(jim).
 homem(hiusu).
-
 homem(gabriel).
-
 homem(guga).
-
 homem(samuel).
-
 homem(montekyo).
 
+genitor(pai, filho).
+genitor(mae, filho).
+genitor(pai, filha).
+genitor(mae, filha).
+genitor(pam, bob).
+genitor(tom, bob).
+genitor(tom, liz).
+genitor(bob, ann).
+genitor(bob, pat).
+genitor(pat, jim).
+genitor(gabriel, hiusu).
+genitor(madalena, hiusu).
+genitor(gabriel, guga).
+genitor(madalena, francisca).
+genitor(samuel, duda).
+genitor(samanta, duda).
+genitor(hiusu, montekyo).
+genitor(hiusu, nina).
+genitor(duda, montekyo).
+genitor(duda, nina).
 
+prole(filho, pai).
+prole(filho, mae).
+prole(filha, pai).
+prole(filha, mae).
+prole(Y, X):- genitor(X, Y).
 
+mae(mae, filho).
+mae(mae, filha).
+mae(X, Y):- genitor(X, Y), mulher(X).
 
-homem(tom).
+pai(pai, filho).
+pai(pai, filha).
+pai(X, Y):- genitor(X, Y), homem(X).
 
-homem(bob).
+avos(avo, neto).
+avos(avo, neta).
+avos(X, Y):-genitor(X, Z), genitor(Z, Y).
 
-homem(jim).
+irma(irma, irma).
+irma(irma, irmao).
+irma(X, Y):- genitor(Z, X), genitor(Z, Y), mulher(X), not(X = Y).
 
-prole(Y,X):-genitor(X,Y).
+irmao(irmao, irma).
+irmao(irmao, irmao).
+irmao(X, Y):- genitor(Z, X), genitor(Z, Y), homem(X), not(X = Y).
 
-mae(X,Y):-genitor(X,Y), mulher(X).
-pai(X,Y):-genitor(X,Y), homem(X).
+descendente(descendente, ascendente).
+descendente(X, Z):- genitor(X, Z).
+descendente(X, Z):- genitor(X, Y), descendente(Y, Z).
 
-avos(X,Y):-genitor(X,Z), genitor(Z,Y).
+ascendente(ascendente, descendente).
+ascendente(X, Y):- genitor(Y, X).
+ascendente(X, Y):- genitor(Y, Z), genitor(Z, X).
 
-irma(X,Y):-genitor(Z,X), genitor(Z,Y), mulher(X), not(x=y).
+tio(tio, sobrinho).
+tio(tio, sobrinha).
+tio(X, Y):- irmao(X, Z), genitor(Z, Y), not(X = Z).
 
-descendente(X,Z):-genitor(X,Z).
-descendente(X,Z):-genitor(X,Y), descendente(Y,Z).
+tia(tia, sobrinho).
+tia(tia, sobrinha).
+tia(X, Y):- irma(X, Z), genitor(Z, Y), not(X = Z).
 
+avospaternos(avo, avo, neto).
+avospaternos(avo, avo, neta).
+avospaternos(X, Y, Z):-genitor(X, W), genitor(Y, W), genitor(W, Z), homem(W), homem(X), mulher(Y).
 
+avosmaternos(avo, avo, neto).
+avosmaternos(avo, avo, neta).
+avosmaternos(X, Y, Z):-genitor(X, W), genitor(Y, W), genitor(W, Z), mulher(W), homem(X), mulher(Y).
 
-
-
-irmao(X,Y):-genitor(Z,X), genitor(Z,Y), homem(X), not(x=y).
-
-tio(X,Y):-irmao(X,Z), genitor(Z,Y), not(X=Z).
-
-tia(X,Y):-irma(X,Z), genitor(Z,Y), not(X=Z).
-
-avospaternos(X,Y,Z):-genitor(X,W), genitor(Y,W), genitor(W,Z), homem(W), homem(X), mulher(Y).
-
-avosmaternos(X,Y,Z):-genitor(X,W), genitor(Y,W), genitor(W,Z), mulher(W), homem(X), mulher(Y).
-
-ascendente(X,Y):-genitor(Y,X).
-
-ascendente(X,Y):-genitor(Y,Z), genitor(Z,X).
